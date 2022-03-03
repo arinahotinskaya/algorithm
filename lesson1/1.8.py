27755d72d358d6baef281d3e9ef8f6e48630b1a8
arinahotinskaya <75490835+arinahotinskaya@users.noreply.github.com>
2022-03-03 10:40:55 +0300

    Update 1.8.py

--- lesson1/1.8.py
+++ lesson1/1.8.py
@@ -3,10 +3,7 @@
 print("Введите любой год: ")
 year = int(input())
 
-if year <= 0:
-    print("Год меньше нуля, попробуйте снова!")
-
-if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+if year % 4 == 0 and year % 100 != 0 and year > 0 or year % 400 == 0 and year > 0:
     print(f"Год {year} - високосный")
 else:
     print(f"Год {year} - невисокосный")
